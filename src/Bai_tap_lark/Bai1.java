d4a5e3807a5f4768559a337753138b928a85d7fa
Pham Trung Hieu <ptrunghieu2507@gmail.com>
2025-03-10 10:53:31 +0700

    bai tap trong lark

--- src/Bai_tap_lark/Bai1.java
+++ src/Bai_tap_lark/Bai1.java
@@ -40,7 +40,7 @@ public class Bai1 {
                     int sum = 0;
                     count = 0;
                     for (int i = 0; i < arr.length; i++) {
-                        if(arr[i] > 0) {
+                        if (arr[i] > 0) {
                             sum += arr[i];
                             count++;
                         }
@@ -52,7 +52,7 @@ public class Bai1 {
                     int k = sc.nextInt();
                     System.out.print("Vi tri cac phan tu bang k: ");
                     for (int i = 0; i < arr.length; i++) {
-                        if(arr[i] == k) {
+                        if (arr[i] == k) {
                             System.out.print(i + " ");
                         }
                     }
@@ -94,8 +94,102 @@ public class Bai1 {
                     System.out.println("So luong so nguyen to: " + count);
                     break;
                 case 7:
+                    // Phân loại vị trí các nhóm trong mảng
+                    int[] temp = new int[arr.length];
+                    int idx = 0;
+
+                    // Đưa chẵn chia hết cho 3 vào đầu mảng
+                    for (int i = 0; i < arr.length; i++) {
+                        if (arr[i] % 3 == 0 && arr[i] % 2 == 0) {
+                            temp[idx++] = arr[i];
+                        }
+                    }
+
+                    // Đưa các phần tử còn lại vào giữa
+                    for (int i = 0; i < arr.length; i++) {
+                        if (!(arr[i] % 3 == 0)) {
+                            temp[idx++] = arr[i];
+                        }
+                    }
+
+                    // Đưa lẻ chia hết cho 3 vào cuối mảng
+                    for (int i = 0; i < arr.length; i++) {
+                        if (arr[i] % 3 == 0 && arr[i] % 2 != 0) {
+                            temp[idx++] = arr[i];
+                        }
+                    }
+
+                    // Copy lại vào mảng chính
+                    for (int i = 0; i < arr.length; i++) {
+                        arr[i] = temp[i];
+                    }
+
+                    // Sắp xếp nhóm 1 (chẵn chia hết cho 3) tăng dần
+                    int end1 = 0;
+                    while (end1 < arr.length && arr[end1] % 3 == 0 && arr[end1] % 2 == 0) end1++;
+                    for (int i = 0; i < end1 - 1; i++) {
+                        for (int j = i + 1; j < end1; j++) {
+                            if (arr[i] > arr[j]) {
+                                int tempVal = arr[i];
+                                arr[i] = arr[j];
+                                arr[j] = tempVal;
+                            }
+                        }
+                    }
+
+                    // Sắp xếp nhóm 2 (phần còn lại) tăng dần
+                    int start2 = end1, end2 = arr.length;
+                    while (start2 < arr.length && arr[start2] % 3 != 0) start2++;
+                    for (int i = end1; i < start2 - 1; i++) {
+                        for (int j = i + 1; j < start2; j++) {
+                            if (arr[i] > arr[j]) {
+                                int tempVal = arr[i];
+                                arr[i] = arr[j];
+                                arr[j] = tempVal;
+                            }
+                        }
+                    }
+
+                    // Sắp xếp nhóm 3 (lẻ chia hết cho 3) giảm dần
+                    for (int i = start2; i < arr.length - 1; i++) {
+                        for (int j = i + 1; j < arr.length; j++) {
+                            if (arr[i] < arr[j]) {
+                                int tempVal = arr[i];
+                                arr[i] = arr[j];
+                                arr[j] = tempVal;
+                            }
+                        }
+                    }
+
+                    // In mảng kết quả
+                    System.out.println("Mang sau khi sap xep theo yeu cau dac biet:");
+                    for (int i = 0; i < arr.length; i++) {
+                        System.out.print(arr[i] + " ");
+                    }
+                    System.out.println();
                     break;
                 case 8:
+                    System.out.print("Nhap gia tri m can chen: ");
+                    int m = sc.nextInt();
+                    int[] newArr = new int[arr.length + 1];
+                    boolean inserted = false;
+                    int j = 0;
+                    for (int i = 0; i < arr.length; i++) {
+                        if (!inserted && arr[i] < m) {
+                            newArr[j++] = m;
+                            inserted = true;
+                        }
+                        newArr[j++] = arr[i];
+                    }
+                    if (!inserted) {
+                        newArr[j] = m;
+                    }
+                    arr = newArr;
+                    System.out.println("Mang sau khi chen " + m + " vao:");
+                    for (int i = 0; i < arr.length; i++) {
+                        System.out.print(arr[i] + " ");
+                    }
+                    System.out.println();
                     break;
                 case 9:
                     System.out.print("Da thoat chuong trinh!");
--- src/Bai_tap_lark/Bai2.java
+++ src/Bai_tap_lark/Bai2.java
@@ -66,6 +66,24 @@ public class Bai2 {
                     System.out.println("\nTong: " + sum);
                     break;
                 case 5:
+                    for (int j = 0; j < m; j++) {
+                        for (int i = 0; i < n - 1; i++) {
+                            for (int k = i + 1; k < n; k++) {
+                                if (arr[i][j] > arr[k][j]) {
+                                    int temp = arr[i][j];
+                                    arr[i][j] = arr[k][j];
+                                    arr[k][j] = temp;
+                                }
+                            }
+                        }
+                    }
+                    System.out.println("Mang sau khi sap xep tang dan theo cot:");
+                    for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < m; j++) {
+                            System.out.print(arr[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
                     break;
                 case 6:
                     System.out.println("Cac so nguyen to trong mang:");
@@ -89,8 +107,49 @@ public class Bai2 {
                     System.out.println();
                     break;
                 case 7:
+                    int min = Math.min(n, m);
+                    for (int i = 0; i < min - 1; i++) {
+                        for (int j = i + 1; j < min; j++) {
+                            if (arr[i][i] < arr[j][j]) {
+                                int temp = arr[i][i];
+                                arr[i][i] = arr[j][j];
+                                arr[j][j] = temp;
+                            }
+                        }
+                    }
+                    System.out.println("Mang sau khi sap xep giam dan tren duong cheo chinh:");
+                    for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < m; j++) {
+                            System.out.print(arr[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
                     break;
                 case 8:
+                    System.out.print("Nhap so luong phan tu cua mang 1 chieu: ");
+                    int p = sc.nextInt();
+                    int[] arr1D = new int[p];
+                    System.out.println("Nhap cac phan tu cua mang 1 chieu:");
+                    for (int i = 0; i < p; i++) {
+                        arr1D[i] = sc.nextInt();
+                    }
+
+                    System.out.print("Nhap vi tri hang muon chen (0 - " + (n - 1) + "): ");
+                    int row = sc.nextInt();
+                    if (row >= 0 && row < n) {
+                        for (int j = 0; j < Math.min(m, p); j++) {
+                            arr[row][j] = arr1D[j];
+                        }
+                        System.out.println("Mang sau khi chen:");
+                        for (int i = 0; i < n; i++) {
+                            for (int j = 0; j < m; j++) {
+                                System.out.print(arr[i][j] + " ");
+                            }
+                            System.out.println();
+                        }
+                    } else {
+                        System.out.println("Vi tri hang khong hop le!");
+                    }
                     break;
                 case 9:
                     System.out.print("Da thoat chuong trinh!");
